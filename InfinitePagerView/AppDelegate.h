77ed4f5ad32682400d75098c9b4278395cb371bc
Masahiro Morodomi <morodomi at gmail.com>
2012-04-25 10:04:01 +0900

    Initial Commit

---
+++ InfinitePagerView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InfinitePagerView
+//
+//  Created by 政洋 諸富 on 12/04/25.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
